2d4a1b7dad5f19680f38b25b9ca61bd36a71ec22
liukai <mkh0706@dingtalk.com>
2022-03-22 11:27:29 +0800

    添加readme.md文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,16 @@ module.exports = {
     'airbnb-base',
   ],
   parserOptions: {
-    ecmaVersion: 2022,
+    ecmaVersion: 2020,
     sourceType: 'module',
   },
   plugins: ['vue'],
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+    'vue/setup-compiler-macros': true,
+  },
   rules: {
     // 自己写一些想配置的规则
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off', // 生产环境禁用consele
@@ -17,13 +23,19 @@ module.exports = {
     'vue/max-attributes-per-line': 'off', // 关闭强制 html 标签换行
     'no-param-reassign': ['error', { props: false }], // 允许修改参数中的属性值
     'prefer-destructuring': ['error', { object: true, array: false }], // 允许数组通过下标取值
-    'vue/multi-word-component-names': off,
     'max-len': 'off', // 对象选项
     'no-use-before-define': 'off', // 允许定义之前使用
     'func-names': 'off', // 允许使用匿名函数
-    'no-shadow': ['error', { allow: ['state'] }], // 允许对其进行阴影处理
+    'import/no-unresolved': [2, { ignore: ['^@/'] }], // @ 是设置的路径别名
+    // 去掉store报错情况
+    'no-shadow': ['error', { allow: ['state', 'getters'] }],
+    'no-unused-vars': ['error', { argsIgnorePattern: 'commit' }],
     'import/prefer-default-export': 'off', // 模块只输出一个变量时，是否需要添加default
     'no-plusplus': 'off', // 一元运算符
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-expressions': ['error', { allowShortCircuit: true }], // 允许使用断路表达式
+    'import/extensions': 'off', // 取消对文件扩展名的验证
+    'vue/multi-word-component-names': 'off',
   },
   overrides: [
     {
--- README.md
+++ README.md
@@ -1,3 +1,47 @@
-# kui-next
-# kui-next
-# kui-next
+## kui-next vue3组件库 基于element-ui二次开发，根据项目需求，方便项目开发使用 
+
+# 安装
+```
+# Npm  
+npm install kui-next
+ 
+# Yarn  
+yarn add kui-next 
+```
+## 使用 按需引入
+
+```js
+import {
+  KButton,
+  KInput,
+  KTable,
+  KPage,
+  KBatchTable,
+  KDialog,
+  KBreadcrumb,
+  KTabs,
+  KPicker,
+} from 'kui-next';
+
+export default {
+  components: {
+    KButton,
+    KInput,
+    KTable,
+    KPage,
+    KBatchTable,
+    KDialog,
+    KBreadcrumb,
+    KTabs,
+    KPicker,
+  },
+};
+```
+## 全局components:
+
+```
+import { createApp } from 'vue';
+import { KUI } from 'kui-next';
+ 
+Vue.use(KUI)
+```
